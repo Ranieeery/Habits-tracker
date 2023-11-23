58b39f75198b31a75ae2bc4f2da2904cf099bee5
Ranieeery <raniery2003@hotmail.com>
2023-11-23 03:28:03 -0300

    feat: finish prisma tables

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,11 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 import { PrismaClient } from "@prisma/client";
 
 const server = Fastify();
 const prisma = new PrismaClient();
+
+server.register(cors)
 
 server.get("/", async () => {
     const habits = await prisma.habit.findMany();
