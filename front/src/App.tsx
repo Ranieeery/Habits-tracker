68401f02fcaa4fca55f5c10e90df1f7b642af1c2
Ranieeery <raniery2003@hotmail.com>
2023-11-23 02:48:09 -0300

    feat: tailwind project config

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,19 +1,18 @@
-import { Habit } from "./components/Habit"
+import "./styles/global.css";
+
+import { Habit } from "./components/Habit";
 
 function App() {
-
-
-  return (
-    <div className="App">
-      <Habit completed={2} />
-      <Habit completed={4} />
-      <Habit completed={2} />
-      <Habit completed={2} />
-      <Habit completed={2} />
-      <Habit completed={2} />
-      
-    </div>
-  )
+    return (
+        <div className="App">
+            <Habit completed={2} />
+            <Habit completed={4} />
+            <Habit completed={2} />
+            <Habit completed={2} />
+            <Habit completed={2} />
+            <Habit completed={2} />
+        </div>
+    );
 }
 
-export default App
+export default App;
