07fd8900b7bac2b5fe33f42d74f2fa69d3eb92f8
Ranieeery <raniery2003@hotmail.com>
2025-01-24 15:50:09 -0300

    feat: integrate Tailwind CSS for styling, add Home and Loading components, and update global styles

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { useFonts, Inter_400Regular,  Inter_600SemiBold, Inter_500Medium, Inter_800ExtraBold} from "@expo-google-fonts/inter";
-import { Loading } from "./src/assets/components/Loading";
+import {
+    useFonts,
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_500Medium,
+    Inter_800ExtraBold,
+} from "@expo-google-fonts/inter";
+import { Loading } from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
+import "./styles/global.css";
 
 export default function App() {
     const [fontsLoaded] = useFonts({
@@ -16,22 +23,9 @@ export default function App() {
     }
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+        <>
+            <Home />
             <StatusBar style="light" translucent />
-        </View>
+        </>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#09090A",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    text: {
-        color: "#FFF",
-        fontFamily: "Inter_800ExtraBold",
-    }
-});
