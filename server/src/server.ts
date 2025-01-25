844de2304b6c32a8dbec745c31d1093459d0722c
Ranieeery <raniery2003@hotmail.com>
2025-01-25 20:20:44 -0300

    feat: update server configuration to listen on all interfaces and use a variable for port number

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,14 +3,16 @@ import cors from "@fastify/cors";
 import { appRoutes } from "./routes";
 
 const server = Fastify();
+const port = 3000;
 
 server.register(cors);
 server.register(appRoutes);
 
 server
     .listen({
-        port: 3000,
+        host: "0.0.0.0",
+        port: port,
     })
     .then(() => {
-        console.log("Server listening on http://localhost:3000");
+        console.log(`Server listening on http://localhost:${port}`);
     });
