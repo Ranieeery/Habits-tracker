e6de54723ca263dbf55bfab9416116b6c9e7bfae
Ranieeery <raniery2003@hotmail.com>
2025-01-25 14:56:56 -0300

    feat: add Popover component to HabitDay for enhanced user interaction

--- front/src/components/HabitDay.tsx
+++ front/src/components/HabitDay.tsx
@@ -1,5 +1,24 @@
+import * as Popover from "@radix-ui/react-popover";
+
 export function HabitDay() {
     return (
-        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+        <Popover.Root>
+            <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
+
+            <Popover.Portal>
+                <Popover.Content className="min-w-[320px] w-full p-6 rounded-2xl bg-zinc-900 flex flex-col">
+                    <span className="font-semibold text-zinc-400">Sábado</span>
+                    <span className="font-extrabold mt-1 leading-tight text-3xl">17/01</span>
+
+
+
+                    <Popover.Arrow
+                        height={8}
+                        width={16}
+                        className="fill-zinc-900"
+                    />
+                </Popover.Content>
+            </Popover.Portal>
+        </Popover.Root>
     );
 }
