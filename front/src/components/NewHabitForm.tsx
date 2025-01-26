9be91ee21fbf019689ec53ac0a5fd82f30265ef2
Ranieeery <raniery2003@hotmail.com>
2025-01-26 17:57:32 -0300

    feat: enhance accessibility and focus styles in HabitsList and NewHabitForm components

--- front/src/components/NewHabitForm.tsx
+++ front/src/components/NewHabitForm.tsx
@@ -72,13 +72,13 @@ export function NewHabitForm() {
                     return (
                         <Checkbox.Root
                             key={day}
-                            className="flex items-center gap-3 group"
+                            className="flex items-center gap-3 group focus:outline-none"
                             checked={weekDays.includes(i)}
                             onCheckedChange={() => {
                                 handleToggleWeekDay(i);
                             }}
                         >
-                            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 transition-all duration-200 ease-[ease] group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 transition-all duration-200 ease-[ease] group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background">
                                 <Checkbox.Indicator>
                                     <Check size={20} className="text-white" />
                                 </Checkbox.Indicator>
@@ -94,7 +94,7 @@ export function NewHabitForm() {
 
             <button
                 type="submit"
-                className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 cursor-pointer hover:bg-green-500 transition-colors"
+                className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 cursor-pointer hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
             >
                 <Check size={20} weight="bold" /> Criar hábito
             </button>
