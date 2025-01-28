a60fb13f6a4a0fdff7f4e0c199f686bdd01ce737
Ranieeery <raniery2003@hotmail.com>
2025-01-27 22:32:10 -0300

    feat: improve error handling in NewHabit component by returning early on validation failure

--- mobile/src/screens/NewHabit.tsx
+++ mobile/src/screens/NewHabit.tsx
@@ -40,7 +40,7 @@ export function New() {
     async function handleCreateNewHabit() {
         try {
             if (!title.trim() || weekDays.length === 0) {
-                Alert.alert("Erro", "O título e hábitos são obrigatórios");
+                return Alert.alert("Erro", "O título e hábitos são obrigatórios");
             } else {
                 await api.post("/habits", {
                     title,
