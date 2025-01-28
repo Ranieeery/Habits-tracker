4d90a96451766ea636fc99c54dc83fb75c236670
Ranieeery <raniery2003@hotmail.com>
2025-01-28 13:26:18 -0300

    style: format imports in types.ts for improved readability

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,4 +1,10 @@
-import { FastifyBaseLogger, FastifyInstance, RawReplyDefaultExpression, RawRequestDefaultExpression, RawServerDefault } from "fastify";
+import {
+    FastifyBaseLogger,
+    FastifyInstance,
+    RawReplyDefaultExpression,
+    RawRequestDefaultExpression,
+    RawServerDefault,
+} from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 
 export type FastifyTypedInstance = FastifyInstance<
@@ -7,4 +13,4 @@ export type FastifyTypedInstance = FastifyInstance<
     RawReplyDefaultExpression,
     FastifyBaseLogger,
     ZodTypeProvider
->
+>;
