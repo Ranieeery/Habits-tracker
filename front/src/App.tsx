5cf954bc141e9c488ef599eb27ac0191e075ad0e
Ranieeery <raniery2003@hotmail.com>
2025-05-31 23:36:31 -0300

    feat: implement service worker for push notifications

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,6 +4,8 @@ import "./styles/global.css";
 import { Header } from "./components/Header";
 import { SummaryTable } from "./components/SummaryTable";
 
+navigator.serviceWorker.register("service-worker.js");
+
 export function App() {
     return (
         <div className="w-screen h-screen flex justify-center items-center">
