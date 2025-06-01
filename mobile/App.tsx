80c1ca61343138354f1b77514981f854085cfe9c
Ranieeery <raniery2003@hotmail.com>
2025-06-01 01:09:12 -0300

    feat: integrate expo-navigation-bar and update app for push notifications

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -10,6 +10,9 @@ import {
 } from "@expo-google-fonts/inter";
 import { Loading } from "./src/components/Loading";
 import { Routes } from "./src/routes";
+import * as Notifications from "expo-notifications";
+import * as NavigationBar from "expo-navigation-bar";
+import { useEffect } from "react";
 
 export default function App() {
     const [fontsLoaded] = useFonts({
@@ -18,6 +21,11 @@ export default function App() {
         Inter_500Medium,
         Inter_800ExtraBold,
     });
+
+    useEffect(() => {
+        NavigationBar.setBackgroundColorAsync("#000000");
+        NavigationBar.setButtonStyleAsync("light");
+    }, []);
 
     if (!fontsLoaded) {
         return <Loading />;
