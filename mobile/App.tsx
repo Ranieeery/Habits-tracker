8897703a675f7362310e0bfa560ebefe14c4e428
Ranieeery <raniery2003@hotmail.com>
2025-06-01 02:04:23 -0300

    feat: add notification scheduling functionality and permission handling

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -13,6 +13,15 @@ import { Routes } from "./src/routes";
 import * as Notifications from "expo-notifications";
 import * as NavigationBar from "expo-navigation-bar";
 import { useEffect } from "react";
+import { Button, Alert } from "react-native";
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: false,
+    }),
+});
 
 export default function App() {
     const [fontsLoaded] = useFonts({
@@ -22,9 +31,52 @@ export default function App() {
         Inter_800ExtraBold,
     });
 
+    async function requestNotificationPermissions() {
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        
+        if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+        
+        if (finalStatus !== 'granted') {
+            Alert.alert('Erro', 'Permissão de notificação negada!');
+            return false;
+        }
+        
+        return true;
+    }
+
+    async function scheduleNotfication() {
+        const hasPermission = await requestNotificationPermissions();
+        
+        if (!hasPermission) {
+            return;
+        }
+
+        const trigger = new Date(Date.now());
+        trigger.setSeconds(trigger.getSeconds() + 10);
+
+        await Notifications.scheduleNotificationAsync({
+            content: {
+                title: "Hello!",
+                body: "This is a test notification.",
+            },
+            trigger: {
+                type: Notifications.SchedulableTriggerInputTypes.DATE,
+                date: trigger,
+            },
+        });
+        
+        // Alert.alert('Success', 'Notification scheduled successfully!');
+    }
+
     useEffect(() => {
         NavigationBar.setBackgroundColorAsync("#000000");
         NavigationBar.setButtonStyleAsync("light");
+        
+        requestNotificationPermissions();
     }, []);
 
     if (!fontsLoaded) {
@@ -33,6 +85,10 @@ export default function App() {
 
     return (
         <>
+            <Button
+                title="Schedule Notification"
+                onPress={scheduleNotfication}
+            />
             <Routes />
             <StatusBar
                 style="light"
